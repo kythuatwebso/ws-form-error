bbd56921403045a9cdb930e1aabbd2566b640c0c
Hoang Nam <lehoangnam14@gmail.com>
2017-02-18 00:18:31 +0700

    FIX Not run textarea, select

--- ws-formError.js
+++ ws-formError.js
@@ -1,13 +1,18 @@
+/**
+ * HÀM BẮT LỖI TRONG FORM
+ * Gia tri truyen vao la class [ws-required]
+ * Author: Webso Company
+ */
 (function($) {
     $.fn.wsFormError = function(options) {
 
+        // Khai Bao
         var selector = this.selector,
             error    = 0,
-            mess     = '',
+            message  = '',
         opts = $.extend({
             enable: true,
-            select: 'input',
-            message: false
+            notification: false
         }, options);
 
         if ($('body').find(this).length) {
@@ -15,22 +20,13 @@
                 console.warn('option (enable) is not type boolean !');
                 return;
             }
-            if (opts.select == "" || opts.select == null) {
-                console.warn('option (select) is null !');
-                return;
-            }
-            if (typeof(opts.select) !== "string") {
-                console.warn('option (select) is not type string !');
-                return;
-            }
-            if (typeof(opts.message) !== "boolean") {
+            if (typeof(opts.notification) !== "boolean") {
                 console.warn('option (message) is not type boolean !');
                 return;
             }
-
             var viewMessage = function(message) {
                 if (message != "") {
-                    return '<span class="ws-error-mess"'
+                    return '<span class="wserror-message"'
                             +'style="'
                             +'display:block;'
                             +'color:white;'
@@ -46,98 +42,97 @@
                             +'">'+message+'</span>';
                 }
             }
-
             var isEmail = function(emailAddress) {
                 var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
                 return pattern.test(emailAddress);
             }
-
             var isNumber = function(n) {
                 return !isNaN(parseFloat(n)) && isFinite(n);
             }
-
             if (opts.enable) {
-                $(this).find(opts.select).each(function() {
+                // Enable Plugin
+                $(selector).find('input,textarea,select').each(function() {
                     var item     = $(this);
                     var itemval  = item.val();
-                    var itemtype = item.attr('type');
+                    var itemtype = item.prop('nodeName');
+                    // CHECK EMPTY
                     if (item.hasClass('ws-required')) {
-                        $.each(item, function(index, val) {
-                            _item    = $(val);
-                            _itemval = _item.val();
-                            if (!_itemval.length) {
-                                // Require is not null (default check)
-                                error = 1;
-                                _item.addClass('isError');
-                                if (typeof(_item.data('wserror-null')) !== "undefined") {
-                                    mess = _item.data('wserror-null');
-                                } else {
-                                    mess = "Please enter in fields required.";
-                                }
+                        if (!itemval.length) {
+                            // Require is not null (default check)
+                            error = 1;
+                            item.addClass('isError');
+                            if (typeof(item.data('wserror-null')) !== "undefined") {
+                                message = item.data('wserror-null');
                             } else {
-                                _item.removeClass('isError');
+                                message = "Please enter in fields required.";
                             }
-                            if (_item.hasClass('ws-required-number')) {
-                                if (!parseFloat(_itemval) || !isFinite(_itemval)) {
-                                    // require is number (option check)
-                                    error = 1;
-                                    _item.addClass('isError');
-                                    if (typeof(_item.data('wserror-number')) !== "undefined") {
-                                        mess = _item.data('wserror-number');
-                                    }
-                                } else {
-                                    _item.removeClass('isError');
-                                }
+                        } else {
+                            item.removeClass('isError');
+                        }
+                    }
+                    // CHECK NUMBER
+                    if (item.hasClass('ws-required-number')) {
+                        if (!parseFloat(itemval) || !isFinite(itemval)) {
+                            // require is number (option check)
+                            error = 1;
+                            item.addClass('isError');
+                            if (typeof(item.data('wserror-number')) !== "undefined") {
+                                message = item.data('wserror-number');
                             }
-                            if (_item.hasClass('ws-required-email')) {
-                                if (!isEmail(_itemval)) {
-                                    // require is email (option check)
-                                    error = 1;
-                                    _item.addClass('isError');
-                                    if (typeof(_item.data('wserror-email')) !== "undefined") {
-                                        mess = _item.data('wserror-email');
-                                    }
-                                } else {
-                                    _item.removeClass('isError');
-                                }
+                        } else {
+                            item.removeClass('isError');
+                        }
+                    }
+                    // CHECK EMAIL
+                    if (item.hasClass('ws-required-email')) {
+                        if (!isEmail(itemval)) {
+                            // require is email (option check)
+                            error = 1;
+                            item.addClass('isError');
+                            if (typeof(item.data('wserror-email')) !== "undefined") {
+                                message = item.data('wserror-email');
                             }
-                            if (_item.hasClass('ws-required-phone')) {
-                                if ((_itemval.length < 10 || _itemval.length > 11) || (!isNumber(_itemval))) {
-                                    // require is phone (option check)
-                                    error = 1;
-                                    _item.addClass('isError');
-                                    if (typeof(_item.data('wserror-phone')) !== "undefined") {
-                                        mess = _item.data('wserror-phone');
-                                    }
-                                } else {
-                                    _item.removeClass('isError');
-                                }
+                        } else {
+                            item.removeClass('isError');
+                        }
+                    }
+                    // CHECK PHONE
+                    if (item.hasClass('ws-required-phone')) {
+                        if ((itemval.length < 10 || itemval.length > 11) || (!isNumber(itemval))) {
+                            // require is phone (option check)
+                            error = 1;
+                            item.addClass('isError');
+                            if (typeof(item.data('wserror-phone')) !== "undefined") {
+                                message = item.data('wserror-phone');
                             }
-                            // VIEW ERROR
-                            if (_item.hasClass('isError')) {
-                                // Add class has-error when error is 1
-                                    _item.parent('.form-group').addClass('has-error');
-                            } else {
-                                // remove Class has-error
-                                _item.parent('.form-group').removeClass('has-error');
-                            }
-
-                            if (opts.message) {
-                                if (!$('body').find('.ws-error-mess').length) {
-                                    $('body').append(viewMessage(mess));
-                                    setTimeout(function() {
-                                        $('body').find('.ws-error-mess').remove();
-                                    }, 2000);
-                                }
-                            }
-                        });
+                        } else {
+                            item.removeClass('isError');
+                        }
+                    }
+                    // VIEW ERROR
+                    if (item.hasClass('isError')) {
+                        // Add class has-error when error is 1
+                            item.parent('.form-group').addClass('has-error');
+                    } else {
+                        // remove Class has-error
+                        item.parent('.form-group').removeClass('has-error');
+                    }
+                    if (opts.notification) {
+                        if (!$('body').find('.wserror-message').length) {
+                            $('body').append(viewMessage(message));
+                            setTimeout(function() {
+                                $('body').find('.wserror-message').remove();
+                            }, 2000);
+                        }
                     }
                 });
-                return error;
+                var callback = opts.callback;
+                if ($.isFunction(callback)) {
+                    callback.call(this, error);
+                }
             }
         } else {
             console.error("element [ "+selector+" ] is not exists !");
         }
-
     }
 })(jQuery);
